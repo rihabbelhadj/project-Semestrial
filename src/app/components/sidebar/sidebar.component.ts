229bcb924038d1d07cf177f26bd33db7ef12ee71
rihabbelhadj <rihabbelhadj41@gmail.com>
2022-12-08 21:29:43 +0100

    Add existing project files to Git

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/table-list', title: 'Employees',  icon:'content_paste', class: '' },
+    { path: '/typography', title: 'Entry /Exit',  icon:'bubble_chart', class: '' },
+    { path: '/icons', title: 'Calendar',  icon:'calendar_month', class: '' },
+    { path: '/maps', title: 'Request Leave',  icon:'accessibility_new', class: '' },
+
+
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
